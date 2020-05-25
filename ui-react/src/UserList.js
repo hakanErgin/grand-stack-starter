b6d700ebaaa0d81b31ad74881eac1299fe356ff8
hakanErgin <hakan.ergin@gmail.com>
2020-05-25 10:08:59 +0200

    prints username

--- ui-react/src/UserList.js
+++ ui-react/src/UserList.js
@@ -3,13 +3,8 @@ import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
 const GET_USER = gql`
-  query usersPaginateQuery(
-    $first: Int
-    $offset: Int
-    $orderBy: [_UserOrdering]
-    $filter: _UserFilter
-  ) {
-    User(first: $first, offset: $offset, orderBy: $orderBy, filter: $filter) {
+  query {
+    User {
       id
       name
     }
