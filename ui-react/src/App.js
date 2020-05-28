989b10502b99960da7a83da9c3214f43fda114eb
hakanErgin <hakan.ergin@gmail.com>
2020-05-28 12:18:54 +0200

    login register works with jwt token

--- ui-react/src/App.js
+++ ui-react/src/App.js
@@ -1,20 +1,86 @@
-import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
-import { withRouter } from "react-router";
-import Login from "./components/Login";
-import UserList from "./components/UserList";
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { ApolloLink, concat } from "apollo-link";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import injectSheet from "react-jss";
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Switch>
-          <Route exact path="/" component={UserList} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </React.Fragment>
-    );
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import { colors } from "./constants/colors";
+
+const styles = {
+  appContainer: {
+    position: "relative",
+    background: colors.black,
+    minHeight: "100vh",
+    display: "flex",
+    verticalAlign: "middle",
+    alignItems: "center",
+    overflowX: "hidden",
+    maxWidth: "100%"
   }
-}
+};
 
-export default withRouter(App);
+const httpLink = new HttpLink({
+  uri: "http://localhost:4001/graphql"
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext({
+    headers: {
+      authorization: localStorage.getItem("token") || null
+    }
+  });
+
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  link: concat(authMiddleware, httpLink),
+  cache: new InMemoryCache()
+});
+
+const checkAuth = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    return true;
+  } else {
+    const queryParams = new URLSearchParams(window.location.search);
+    const altToken = queryParams.get("token");
+    if (altToken) {
+      localStorage.setItem("token", altToken);
+      return true;
+    }
+    return false;
+  }
+};
+
+const AuthRoute = ({ ...props }) => (
+  <Route
+    {...props}
+    render={() =>
+      checkAuth() ? <Home /> : <Redirect to={{ pathname: "/login" }} />
+    }
+  />
+);
+
+const App = ({ classes }) => (
+  <ApolloProvider client={client}>
+    <div className={classes.appContainer}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <AuthRoute exact path="/" />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  </ApolloProvider>
+);
+
+export default injectSheet(styles)(App);
